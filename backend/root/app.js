98c7181c69792ed027ad02e88dd3a3d8db57d351
shinz2307 <127090101+shinz2307@users.noreply.github.com>
2023-06-19 17:29:24 +0800

    connect to MySQL

--- backend/root/app.js
+++ backend/root/app.js
@@ -1,20 +1,40 @@
 // import dependencies here
 const express = require('express');
-const mysql = require("mysql")
-const dotenv = require('dotenv')
-
+const mysql = require("mysql2");
 
 const app = express();
+const db = mysql.createConnection({
+    user    :   'root',
+    host    :   'localhost',
+    password:   "",
+    database:   "50003_database"
+});
+/*
+app.post('/create', (req,res) => {
+    const name = req.body.landlord_user;
+    const landlord_password1 = req.body.landlord_password1;
+    const landlord_password2 = req.body.landlord_password2;
+    const ticket_type = req.body.landlord_ticket;
 
-dotenv.config({path: './.env'})
-
-//connect database
-db.connect((error) => {
-    if(error) {
-        console.log(error)
+    if (landlord_password1 === landlord_password2) {
+        db.query(
+            'INSERT INTO landlord_user (building_id, username, password, ticket_type) VALUES (1,?,?,?)',
+            [name,landlord_password1,ticket_type],
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.send("Values inserted");
+                }
+            })
     } else {
-        console.log("MySQL connected!")
+        console.log("passwords are not the same")
     }
+    
 })
+*/
 
-// to be continued
+app.listen(3001, () => {
+    console.log("server running on port 3001")
+});
+
