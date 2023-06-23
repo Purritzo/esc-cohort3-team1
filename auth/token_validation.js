12a13e52c1786d7d0a5602290d85ce3a0d43ab71
Carina <carinachu22@gmail.com>
2023-06-24 01:29:02 +0800

    Added comments for jwt token verification

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -1,7 +1,9 @@
 const { verify } = require("jsonwebtoken");
 module.exports = {
   checkToken: (req, res, next) => {
-    let token = req.get("authorization"); //huh?
+    let token = req.get("authorization"); //For now, we manually key in the token generated in authorization section
+
+    // Case 1: Token found
     if (token) {
       token = token.slice(7); //bearer has 6 words and 1 space, so 7th index to get actual token
       verify(token, "qwe1234", (error, decoded) => {
@@ -10,10 +12,18 @@ module.exports = {
             success: 0,
             message: "Invalid token",
           });
+
+          //TOKEN IS VALID
         } else {
-          next(); //call the next method
+          /**What next() does is that it calls the next middleware function
+           * in user.router.js, we havae "router.get("/", checkToken, getUsers);"
+           * Hence, we checkToken first, if its valid, next() moves us to getUsers middleware function
+           */
+          next(); //call the next method OH
         }
-      }); //second argument is the secret key
+      });
+
+      // Case 2: There is no token found in "authorization"
     } else {
       res.json({
         success: 0,
