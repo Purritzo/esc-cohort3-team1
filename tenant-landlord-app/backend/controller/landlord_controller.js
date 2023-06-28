4a4c3e0a2b5ff3ba5cde645632731c9ed81d6aec
Carina <carinachu22@gmail.com>
2023-06-28 16:18:46 +0800

    Change username to email, delete tenant controllers

--- tenant-landlord-app/backend/controller/landlord_controller.js
+++ tenant-landlord-app/backend/controller/landlord_controller.js
@@ -1,7 +1,6 @@
 import {
   createLandlord,
-  getLandlordByUsername,
-  createTenant,
+  getLandlordByEmail,
 } from "../models/landlord_model.js";
 import { genSaltSync, hashSync, compareSync } from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -28,14 +27,14 @@ export const controllerCreateLandlord = (req, res) => {
 
 export const controllerLoginLandlord = (req, res) => {
   const body = req.body;
-  getLandlordByUsername(body.username, (err, results) => {
+  getLandlordByEmail(body.email, (err, results) => {
     if (err) {
       console.log(err);
     }
     if (!results) {
       return res.json({
         success: 0,
-        data: "Invalid username or password",
+        data: "Invalid email or password",
       });
     }
 
@@ -56,25 +55,5 @@ export const controllerLoginLandlord = (req, res) => {
         data: "Invalid email or password",
       });
     }
-  });
-};
-
-export const controllerCreateTenant = (req, res) => {
-  const body = req.body;
-  console.log(body);
-  const salt = genSaltSync(10);
-  body.password = hashSync(body.password, salt);
-  createTenant(body, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        success: 0,
-        message: "Database connection error",
-      });
-    }
-    return res.status(200).json({
-      success: 1,
-      data: results,
-    });
   });
 };
